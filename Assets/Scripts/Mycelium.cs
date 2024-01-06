83506c6f3ffab74c64efcdf1d15bf8c416b443c4
toxazol <antonmelni20@gmail.com>
2024-01-06 13:13:20 +0900

    first algo

--- Assets/Scripts/Mycelium.cs
+++ Assets/Scripts/Mycelium.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Mycelium : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float segLen = 1.0F;
+    public float wiggleRange = 0.5f;
+
+    public float branchProb = 0.99f;
+
+    LineRenderer lineRenderer;
+    Vector2 curVec;
+    
     void Start()
     {
-        
+        lineRenderer = GetComponent<LineRenderer>();
+        curVec = Vector2.up;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        curVec = Wiggle(curVec);
+
+        var lastPoint = lineRenderer.GetPosition(lineRenderer.positionCount - 1);
+        Vector3 newDir = curVec * segLen;
+        lineRenderer.SetPosition(lineRenderer.positionCount++, lastPoint + newDir);
+        // branching recursion
+        if(Random.value < branchProb) return; //it's frame and seglen dependent which is bad
+        Instantiate(this.gameObject, this.transform);
+
+    }
+
+    Vector2 Wiggle(Vector2 vec) {
+        vec.x += Random.Range(-wiggleRange, wiggleRange);
+        vec.y += Random.Range(-wiggleRange, wiggleRange);
+        return vec.normalized;
     }
 }
