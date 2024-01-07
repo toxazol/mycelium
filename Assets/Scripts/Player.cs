a87334f02ee4fae570e17d735d8a75b8a50e43a6
toxazol <antonmelni20@gmail.com>
2024-01-07 20:48:09 +0900

    added boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,20 +8,54 @@ public class Player : MonoBehaviour
 
     public InputAction move;
     public InputAction interact;
+    public bool isMoving = false;
+    public float moveSpeed = 5f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D moveFilter;
 
-    public float inputSensivity = 0.1f;
+    private Rigidbody2D rb;
+    private List<RaycastHit2D> castCollisions = new();
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveAmount = move.ReadValue<Vector2>();
-        this.transform.position += moveAmount * inputSensivity;
+        
+    }
+
+    void FixedUpdate() {
+        var moveAmount = move.ReadValue<Vector2>();
+        if (TryMove(moveAmount)
+            || TryMove(new Vector2(moveAmount.x, 0)) // for player not to get stuck
+            || TryMove(new Vector2(0, moveAmount.y))) // when moving diagonally
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+
+    bool TryMove(Vector2 dir)
+    {
+        if (dir == Vector2.zero) return false;
+
+        int collisionCount = rb.Cast(
+            dir,
+            moveFilter,
+            castCollisions,
+            moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+        if (collisionCount > 0) return false;
+
+        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * dir);
+        return true;
     }
 
     public void OnEnable()
