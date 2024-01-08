55fbe1220e0f008531bed012580a5c91dc2509ab
toxazol <antonmelni20@gmail.com>
2024-01-08 23:20:56 +0900

    combined colliders

--- Assets/Dino.cs
+++ Assets/Dino.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Dino : MonoBehaviour
 {
+    public Vector2 jumpVec;
+    public float jumpInterval; 
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private float jumpTimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("isJumping", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void FixedUpdate() {
+        
+        jumpTimer += Time.fixedDeltaTime;
+
+        if(jumpTimer > jumpInterval) {
+            rb.AddForce(jumpVec);
+            jumpTimer = 0f;
+        }
         
     }
 }
