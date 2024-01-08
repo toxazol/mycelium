44db48807614e9e6332e1b978dd381f2df9e213c
toxazol <antonmelni20@gmail.com>
2024-01-08 22:06:44 +0900

    better colliders and new sprites

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -15,12 +16,16 @@ public class Player : MonoBehaviour
     public float collisionOffset = 0.05f;
     public ContactFilter2D moveFilter;
     public GameObject menuUI;
+    public TextMeshProUGUI plantBtnText;
+    public TextMeshProUGUI symbiosisBtnText;
     private bool isPaused = false; 
     private Rigidbody2D rb;
     public List<RaycastHit2D> castCollisions = new();
     public List<GameObject> dinoFood;
     public float foodOffsetUp = 0.1f;
-
+    public int treeAboveId = 0;
+    public int plantsCount = 0;
+    Dictionary<int, bool> symbiosisTreeIds = new(); // treeId -> isAvailable
     
 
     public void Awake()
@@ -44,15 +49,50 @@ public class Player : MonoBehaviour
 
     public void OnGrow()
     {
-        if(TryMove(Vector2.up, true)) {
-            return;
+        if(TryMove(Vector2.up, true)) return; // not colliding with surface
+        if(symbiosisTreeIds.Count == 0) return;
+
+        bool isTreeAvailable = false;
+        List<int> keys = new List<int>(symbiosisTreeIds.Keys);
+        foreach(var key in keys) {
+            if(!symbiosisTreeIds[key]) continue; // is not available
+
+            isTreeAvailable = true;
+            symbiosisTreeIds[key] = false; // tree not available anymore
+            break;
         }
+        if(!isTreeAvailable) return;
+
         int i = Random.Range(0, dinoFood.Count);
         var foodPos = new Vector3(
             this.transform.position.x, 
             this.transform.position.y + foodOffsetUp, 
             this.transform.position.z);
+
         GameObject.Instantiate(dinoFood[i], foodPos, Quaternion.identity);
+        plantsCount++;
+        plantBtnText.SetText("Plant(" +plantsCount+ ")");
+    }
+
+    public void OnSymbiosis()
+    {
+        if(treeAboveId == 0) return;
+        if(symbiosisTreeIds.TryGetValue(treeAboveId, out _)) return;
+
+        symbiosisTreeIds.Add(treeAboveId, true);
+        symbiosisBtnText.SetText("Symbiosis(" +symbiosisTreeIds.Count+ ")");
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.gameObject.tag == "tree") {
+            treeAboveId = other.gameObject.GetInstanceID();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag == "tree") {
+            treeAboveId = 0;
+        }
     }
     
     // Start is called before the first frame update
