f413bf498b8bb912f203d37d8952d59c1e80bc3a
kaishain <antonmelni20@gmail.com>
2024-01-22 17:53:32 +0900

    scaling drawer in progress

--- Assets/Scripts/ButtonDrawer.cs
+++ Assets/Scripts/ButtonDrawer.cs
@@ -5,41 +5,42 @@ using UnityEngine.UI;
 
 public class ButtonDrawer : MonoBehaviour
 {
-    public float speed = 1f;
-    public float fillStep = 0.01f;
-    public float moveStep = 0.01f;
-    public bool isShow = false;
-    public bool isHide = false;
+    public float moveStep = 0.001f;
+    public bool isShowing = false;
+    public bool isHiding = false;
+    public RectTransform counter;
 
-    Button btn;
     Image img;
-    float step;
     float timer;
     float initFill;
+    Vector2 initPos;
+    float cliffX;
+    float initDist;
     
 
     void Start()
     {
-        btn = GetComponent<Button>();
         img = GetComponent<Image>();
         initFill = img.fillAmount;
+        initPos = GetComponent<RectTransform>().anchoredPosition;
+        cliffX = counter.anchoredPosition.x;
+
+        initDist = Mathf.Abs(initPos.x - cliffX); 
     }
 
     [ContextMenu("Show")]
     public void Show()
     {
-        isShow = true;
-        isHide = false;
-        step = 1/speed;
+        isShowing = true;
+        isHiding = false;
         timer = 0f;
     }
 
     [ContextMenu("Hide")]
     public void Hide()
     {
-        isHide = true;
-        isShow = false;
-        step = 1/speed;
+        isHiding = true;
+        isShowing = false;
         timer = 0f;
     }
 
@@ -48,23 +49,29 @@ public class ButtonDrawer : MonoBehaviour
     void Update()
     {   
 
-        if(!isShow && !isHide) return;
-        timer += Time.deltaTime;
-        if(timer < step) return;
-        
-        timer = 0f;
-        if(isShow && img.fillAmount + fillStep > 1f) {
+        if(!isShowing && !isHiding) return;
+        // dist = init - curProgress*init
+        var btnRight = GetComponent<RectTransform>().anchoredPosition.x;
+
+        if(isShowing && btnRight < cliffX) { // && img.fillAmount + fillStep > 1f
+            GetComponent<RectTransform>().anchoredPosition.Set(cliffX, initPos.y);
             img.fillAmount = 1f;
-            isShow = false;
+            isShowing = false;
             return;
         }
-        if(isHide && img.fillAmount - fillStep < initFill) {
+        if(isHiding && btnRight >= initPos.x) { // && img.fillAmount - fillStep < initFill
+            GetComponent<RectTransform>().anchoredPosition = initPos;
             img.fillAmount = initFill;
-            isHide = false;
+            isHiding = false;
             return;
         }
-        float sign = isShow ? 1 : -1;
-        img.fillAmount += fillStep * sign;
+
+        var dist = Mathf.Abs(btnRight - cliffX);
+
+        var curProgress = (initDist - dist)/initDist;
+
+        float sign = isShowing ? 1 : -1;
+        img.fillAmount = curProgress;
         this.transform.Translate(new Vector3(-moveStep * sign, 0f, 0f));
     }
 }
